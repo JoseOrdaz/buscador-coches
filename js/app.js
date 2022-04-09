79890c9e7a030663a134d780290c215bce630dd0
JoseOrdaz <joseordazsuay@gmail.com>
2022-04-09 17:51:45 +0200

    Resultados y selector año

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 //Variables
 const resultado = document.querySelector('#resultado');
+const year = document.querySelector('#year');
+
+const max = new Date().getFullYear();
+const min = max - 10;
 
 
 //Eventos
 document.addEventListener('DOMContentLoaded', () => {
-    mostrarCoches();
+    mostrarCoches(); //Mostrar los coches
+    llenarSelect(); //Llenar  los años
+
 });
 
 //Funciones
@@ -14,10 +20,23 @@ function mostrarCoches() {
 
         const {marca, modelo, year, puertas, transmision, precio, color} = coche;
         const autoHTML = document.createElement('p');
-        autoHTML.textContent = `${marca} - ${modelo} - ${year} - ${puertas} - ${transmision} - ${precio} - ${color}`;
+        autoHTML.textContent = `${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmisión: ${transmision} - ${precio}€ - Color: ${color}`;
 
         //Insertar en el HTML
         resultado.appendChild(autoHTML)
     })
+
+}
+
+//Llenar el select
+
+function llenarSelect() {
+
+    for (let i = max; i > min; i-- ){
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion);
+    }
 
 }
